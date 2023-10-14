7892429d1bc4bcf9ea278f69e2094481077f086c
simon <2942713658@qq.com>
2023-10-14 13:54:53 +0800

    项目结构初始化

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	g := gin.Default()
+
+	g.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	g.Run()
+}
